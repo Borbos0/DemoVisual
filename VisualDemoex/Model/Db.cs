b068a3d0ea6fa5fa3a8f29b4765a6b093fed17ea
Ермолин Даниил Александрович <10180032@college.local>
2021-10-07 13:58:05 +0300

    Добавьте файлы проекта.

---
+++ VisualDemoex/Model/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualDemoex
+{
+    static class Db
+    {
+        public static DemoForVisualEntities demoForVisualEntities = new DemoForVisualEntities();
+    }
+}
