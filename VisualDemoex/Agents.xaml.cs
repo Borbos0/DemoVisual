a3a19da1998c5415e22b09a871bd10fc1b46c8f3
0 Borbos <nbmvc021@mail.ru>
2021-10-07 16:00:46 +0300

    PreAlpha

--- VisualDemoex/Agents.xaml.cs
+++ VisualDemoex/Agents.xaml.cs
@@ -24,11 +24,31 @@ namespace VisualDemoex
         {
             InitializeComponent();
             AgentList.ItemsSource = Db.demoForVisualEntities.Agent.ToList();
+            CbFilter.Items.Add("Фильтрация");
             CbFilter.Items.Add("По типу");
+            CbSort.Items.Add("Сортировка");
             CbSort.Items.Add("От А до Я");
             CbSort.Items.Add("От Я до А");
+            switch (CbSort.SelectedIndex)
+            {
+                case 0: 
+                    break;
+                case 1:
+                    AgentList.ItemsSource = Db.demoForVisualEntities.Agent.OrderBy(x => x.Title);
+                    break;
+                case 2:
+                    AgentList.ItemsSource = Db.demoForVisualEntities.Agent.OrderByDescending(x => x.Title);
+                    break;
+                default: 
+                    break;
+            }
+            
         }
-
+        /// <summary>
+        /// Сортировка по буквам вводимых пользователем
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TextSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             AgentList.ItemsSource = Db.demoForVisualEntities.Agent.Where(x => x.Title.StartsWith(TextSearch.Text)).ToList();
